8d96a974b56cd1aed344a7f909c2529b7767fb1e
zeoywongzixin <23118969@siswa.um.edu.my>
2026-01-21 15:49:02 +0800

    update L7Q2.java

---
+++ lab/Lab7/src/lab7/L7Q2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URLConnection;
+
+public class L7Q2 {
+    public static void main(String[] args) {
+        
+
+        try {
+        URL u = new URL("http://fsktm.um.edu.my");
+        URLConnection cnn = u.openConnection();
+        InputStream stream = cnn.getInputStream();
+        Scanner in = new Scanner(stream);
+        
+        PrintWriter out = new PrintWriter("index.htm");
+            
+            while(in.hasNextLine()){
+                String line = in.nextLine();
+                out.println(line);
+            
+            }
+            in.close();
+            out.close();
+            System.out.println("Web page saved to index.htm");
+        }
+        catch (IOException e) {
+        System.out.println("IO Error:" + e.getMessage());
+        }
+
+
+
+}
+
+}
+
